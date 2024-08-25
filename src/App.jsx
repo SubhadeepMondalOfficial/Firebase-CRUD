bd343a37cc2f28b2248df6559615f2853b1a2bca
SubhaDeep Mondal <subhadeepmondalofficial@gmail.com>
2024-08-25 17:53:10 +0530

    Read Data From Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import firebaseConfigApp from "./config/firebase-config";
-import { getFirestore, addDoc, collection } from "firebase/firestore";
+import { getFirestore, addDoc, collection, getDocs } from "firebase/firestore";
 import Swal from "sweetalert2";
+import { InfinitySpin } from "react-loader-spinner";
 
 //Firebase db connection
 const db = getFirestore(firebaseConfigApp);
@@ -14,6 +15,8 @@ const App = () => {
     joiningDate: "",
   };
   const [formData, setFormData] = useState(model);
+  const [employeesData, setEmployeesData] = useState([]);
+  const [loader, setLoader] = useState(false);
 
   function handleFormData(event) {
     // console.log(event.target)
@@ -26,23 +29,48 @@ const App = () => {
   const submitFormHandler = async (event) => {
     event.preventDefault();
     try {
+      setLoader(true);
       const docRef = await addDoc(collection(db, "employees"), formData);
       Swal.fire({
         icon: "success",
         title: "Success",
         text: `Employee Added Successfully Id ${docRef.id}`,
       });
-      //after successfully entry clear the form data
-      setFormData(model);
-      
+      readDataFromDB(); //refresh the employee list in UI
+      setLoader(false);
+      setFormData(model); //after successfully entry clear the form data
+
     } catch (error) {
       Swal.fire({
         icon: "error",
         title: "Failed !!!",
         text: error.message,
       });
+      setLoader(false);
     }
   };
+
+  //! Read Data From Firebase
+  const readDataFromDB = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "employees"));
+      const temp = [];
+      querySnapshot.forEach((doc) => {
+        // console.log(`${doc.id} => ${doc.data()}`)
+        const document = doc.data();
+        temp.push(document);
+      });
+      setEmployeesData(temp);
+      console.log(temp);
+      
+    } catch (error) {
+      console.log("Failed to Fetch Data from DB, Error => ", error.message);
+    }
+  };
+
+  useEffect(() => {
+    readDataFromDB();
+  }, []);
 
   return (
     <div>
@@ -58,6 +86,7 @@ const App = () => {
             type="text"
             id="employeeName"
             name="employeeName"
+            required
             value={formData.employeeName}
             onChange={handleFormData}
           />
@@ -67,6 +96,7 @@ const App = () => {
             type="number"
             id="salary"
             name="salary"
+            required
             value={formData.salary}
             onChange={handleFormData}
           />
@@ -76,11 +106,23 @@ const App = () => {
             type="date"
             id="date"
             name="joiningDate"
+            required
             value={formData.joiningDate}
             onChange={handleFormData}
           />
 
-          <input type="submit" value="Submit" />
+          {loader ? (
+            <div>
+              <InfinitySpin
+                visible={true}
+                width="100"
+                color="blueviolet"
+                ariaLabel="infinity-spin-loading"
+              />
+            </div>
+          ) : (
+            <button>Submit</button>
+          )}
         </form>
 
         {/* Employees Details in Table */}
@@ -96,18 +138,14 @@ const App = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>Er Subhadeep</td>
-                <td>8000</td>
-                <td>24-08-2024</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Er Arijit</td>
-                <td>15000</td>
-                <td>02-08-2014</td>
-              </tr>
+              {employeesData.map((item, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item.employeeName}</td>
+                  <td>{item.salary}</td>
+                  <td>{item.joiningDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </aside>
